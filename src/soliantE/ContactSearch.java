59f0ec6b6168e4f8f7d5e3a63a3da8fef1b94af5
itsnotafake <itsnotafake@gmail.com>
2016-07-07 16:55:56 -0700

    Finished up the work, the last thing remaining is fixing the FileReader

--- src/soliantE/ContactSearch.java
+++ src/soliantE/ContactSearch.java
@@ -2,36 +2,100 @@ package soliantE;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.ListIterator;
 
 public class ContactSearch {
 	
-	public static LinkedList<String> contactLL;
-	public static HashMap<String,String> contactHM;
+	private static HashMap<String, List<String>> contactByLastNameHM;
+	private static HashMap<String,String> contactByEmailHM;
+	private static String contactFormat;
 	
-	//contactFormatDetails
-	public static String first_Detail;
-	public static String second_Detail;
-	public static String third_Detail;
-	public static String fourth_Detail;
+	public static void setContactFormat(String s){
+		contactFormat = s;
+	}
 	
-	/*public static void contactFormat(String line){
-		String[] line_array = line.split(",");
-		first_Detail = line_array[0];
-		second_Detail = line_array[1];
-		third_Detail = line_array[2];
-		fourth_Detail = line_array[3];
-	}*/
+	public static void createMaps(){
+		HashMap<String, List<String>> firstMap = new HashMap<String, List<String>>();
+		contactByLastNameHM = firstMap;
+		HashMap<String, String> secondMap = new HashMap<String,String>();
+		contactByEmailHM = secondMap;
+	}
 	
 	public static void addToMaps(String line){
 		String[] line_array = line.split(",");
-		//String first_name = line_array[0];
-		//String last_name = line_array[1];
-		//String email = line_array[2];
-		//String phone = line_array[3];
-		contactHM.put(line_array[2], line);
-		ListIterator<String> li = contactLL.listIterator(0);
-		whil
+		//String first_name = line_array[0], String last_name = line_array[1]
+		//String email = line_array[2], String phone = line_array[3];
+		
+		//This allows for O(1) lookup time by email address
+		contactByEmailHM.put(line_array[2], line);
+		
+		String first_character = line_array[1].substring(0,1);
+		first_character = first_character.toUpperCase();//In case somebody enters the last name in lower case
+		//no last names starting with this particular character have been added yet
+		if(!contactByLastNameHM.containsKey(first_character)){
+			List<String> lastNameList = new LinkedList<String>();
+			lastNameList.add(line);
+			contactByLastNameHM.put(first_character, lastNameList);
+		}else{
+			List<String> lastNameList = contactByLastNameHM.get(first_character);
+			//delete our current entry of the LList as we are going to edit the LList then re-add it
+			contactByLastNameHM.remove(first_character);
+			ListIterator<String> listIterator = lastNameList.listIterator();
+			boolean added = false;
+			int linkedList_index = 0;
+			//this is done to sort the entries by last name in the stored LList
+			while(listIterator.hasNext() && added == false){
+				String current = listIterator.next();
+				String[] current_array = current.split(",");
+				if(line_array[1].compareTo(current_array[1]) < 0){
+					lastNameList.add(linkedList_index, line);
+					added = true;
+				}
+				linkedList_index++;
+			}
+			if(added == false){
+				lastNameList.add(line);
+			}
+			contactByLastNameHM.put(first_character, lastNameList);
+		}
+	}
+	
+	public static void retrieveContactByEmail(String email){
+		try{
+			String line =  contactByEmailHM.get(email);
+			printContact(line);
+		}catch(NullPointerException e){
+			System.out.println("This email does not exist");
+		}
+	}
+	
+	public static void retrieveContactsByLastName(String letter){
+		if(letter.length() > 1){
+			letter = letter.substring(0,1);
+		}
+		letter = letter.toUpperCase();
+		try{
+			List<String> lastNameList = contactByLastNameHM.get(letter);
+			ListIterator<String> listIterator = lastNameList.listIterator();
+			String line = "";
+			while(listIterator.hasNext()){
+				line = listIterator.next();
+				printContact(line);
+			}
+		}catch(NullPointerException e){
+			System.out.println("There are no users whose last name starts with this character");
+		}
+	}
+	
+	public static void printContact(String line){
+		String[] line_array = line.split(",");
+		String first_name = line_array[0];
+		String last_name = line_array[1];
+		String email = line_array[2];
+		String phone = line_array[3];
+		System.out.println("Last: "+last_name+", First: "+first_name+ 
+				", Phone: "+phone+", E-Mail: "+email);
 	}
 
 }
--- src/soliantE/Main.java
+++ src/soliantE/Main.java
@@ -14,16 +14,26 @@ public class Main {
 			FileReader file_Reader = new FileReader(file);
 			BufferedReader buffered_Reader = new BufferedReader(file_Reader);
 			try{
-				//ContactSearch.contactFormat(line = buffered_Reader.readLine());
-				System.out.println(line = buffered_Reader.readLine());
+				//assuming that the first line always contains "first_name,last_name,email,phone"
+				//this try-catch reads that line in as "contact format"
+				ContactSearch.setContactFormat(line = buffered_Reader.readLine());
+				System.out.println(line);
 			}catch(NullPointerException npe){
 				System.out.println("NullPointerException: Cannot determine Contact Format");
 			}
+			//initializes the 2 HashMaps that will be used for storing information
+			ContactSearch.createMaps();
+			
+			//calls a function that reads the line into both HashMaps
 			while((line = buffered_Reader.readLine()) != null){
 				System.out.println(line);
-				//ContactSearch.addToMaps(line);
+				ContactSearch.addToMaps(line);
 			}
 			buffered_Reader.close();
+			
+			//The two different methods for lookup
+			//ContactSearch.retrieveContactsByLastName("s"); //(Currently does not take lower case letters)
+			//ContactSearch.retrieveContactByEmail("LisaESauceda@armyspy.com");
 		}catch(FileNotFoundException fnfe){
 			System.out.println("FileNotFoundException: File could not be found");
 		}catch(IOException ioe){
