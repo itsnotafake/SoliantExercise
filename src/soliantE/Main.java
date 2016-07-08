468198d7562652d222aeddc6d63476c30789a319
itsnotafake <itsnotafake@gmail.com>
2016-07-07 20:17:15 -0700

    Finished the exercvse. Everything works properly now

--- src/soliantE/Main.java
+++ src/soliantE/Main.java
@@ -10,26 +10,27 @@ public class Main {
 	public static void main(String[] Args){
 		String line = null;
 		try{
-			File file = new File(Args[0]);
-			FileReader file_Reader = new FileReader(file);
-			BufferedReader buffered_Reader = new BufferedReader(file_Reader);
+			String fileName = "src\\soliantE\\SampleText";
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			try{
 				//assuming that the first line always contains "first_name,last_name,email,phone"
 				//this try-catch reads that line in as "contact format"
-				ContactSearch.setContactFormat(line = buffered_Reader.readLine());
+				ContactSearch.setContactFormat(line = bufferedReader.readLine());
 				System.out.println(line);
 			}catch(NullPointerException npe){
 				System.out.println("NullPointerException: Cannot determine Contact Format");
 			}
 			//initializes the 2 HashMaps that will be used for storing information
+			//lastName indexing currently has O(n) time but can be optimized for O(logn)
 			ContactSearch.createMaps();
 			
 			//calls a function that reads the line into both HashMaps
-			while((line = buffered_Reader.readLine()) != null){
+			while((line = bufferedReader.readLine()) != null){
 				System.out.println(line);
 				ContactSearch.addToMaps(line);
 			}
-			buffered_Reader.close();
+			bufferedReader.close();
 			
 			//The two different methods for lookup
 			//ContactSearch.retrieveContactsByLastName("s"); //(Currently does not take lower case letters)
